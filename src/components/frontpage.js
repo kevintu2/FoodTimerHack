7df286ba1607b2523b6d806eef492537a4b8ff83
Kevin Tu <57199141+kevintu2@users.noreply.github.com>
2020-11-14 12:31:01 -0500

    MErge
    changes

---
+++ src/components/frontpage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Frontpage extends Component{
+    render(){
+        return(
+            <p>
+            Hello
+            </p>
+        );
+            
+                
+        }
+    }
+
+
+export default Frontpage;
